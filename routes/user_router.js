aebc32ce8c35940dd42c280d2a19d134c49e965f
demiana55samir <65955978+demiana55samir@users.noreply.github.com>
2023-03-31 23:02:20 +0200

    add routes file.

---
+++ routes/user_router.js
@@ -0,0 +1,13 @@
+const{Router}=require('express');
+const userRouter=Router();
+const userController =require('../controllers/user_controllers')
+
+userRouter.get('/',userController.getAllUsers);
+
+userRouter.post('/',userController.addUser);
+userRouter.get('/:nationalId',userController.getUserByNationalId);
+
+
+userRouter.put('/:nationalId',userController.updateInformationById);
+userRouter.delete('/:nationalId',userController.deleteUserByNationalId);
+module.exports={userRouter};
