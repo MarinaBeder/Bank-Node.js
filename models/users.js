40208eb4818b2c0f1e0862a2ef916a033d75bf6e
Marina Beder <Marina180574@feng.bu.edu.eg>
2023-03-31 23:39:19 +0200

    add models

---
+++ models/users.js
@@ -0,0 +1,28 @@
+const users = [
+    {
+        id: 1,
+        nationalId: 11,
+        visaNumber: 11,
+        visaPassword: 11,
+        endVisa: 11,
+        balance: 500,
+        fullName:"Dina Mohamed",
+        address:"cairo",
+        job:"junior engineer",
+        phone:"012"
+
+    },
+    { id: 1, nationalId: 12, visaNumber: 12, visaPassword: 11, endVisa: 11, balance: 600,
+        fullName:"Dina Mohamed",
+    address:"cairo",
+    job:"junior engineer",
+    phone:"012" },
+    { id: 1, nationalId: 13, visaNumber: 13, visaPassword: 11, endVisa: 11, balance: 600,
+        fullName:"Dina Mohamed",
+        address:"cairo",
+        job:"junior engineer",
+        phone:"012" },
+
+
+];
+module.exports = { users };
