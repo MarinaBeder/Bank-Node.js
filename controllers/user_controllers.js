aafb2bb521fc3a726da3aee8a31504ada5f86a01
demiana55samir <65955978+demiana55samir@users.noreply.github.com>
2023-04-14 20:58:29 +0200

    Update user_controllers.js

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,1 +1,16 @@
 const {User} = require('../models/users');
+
+
+
+const getAllUsers = async (req,res)=>{
+    try {const allUsers= await User.find({});
+    res.status(200).send(allUsers);
+        
+    } catch (error) {
+        res.status(400).send(error);
+    }
+    
+};
+module.exports={
+       getAllUsers,
+       };
