25f0d789a3fe1df1258d82017b03dc349161ba75
maryamazab <96701611+maryamazab@users.noreply.github.com>
2023-04-14 22:08:31 +0200

    getUserByNationalId

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,1 +1,22 @@
 const {User} = require('../models/users');
+const getUserByNationalId = async(req,res)=>{
+ 
+    try {
+        const user= await User.findOne({nationalId:req.params.nationalId});
+        if(user==null){
+            res.status(404).send('user not found');
+    
+          }
+          else{res.status(200).send(user)};
+    
+        
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+
+};
+
+module.exports={
+getUserByNationalId,         };
+
