9d13390e9c7e59fa0e3b2892d424bfbb306b09a3
dina-mohamed52 <66019870+dina-mohamed52@users.noreply.github.com>
2023-04-14 23:14:17 +0200

    Update user_controllers.js

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,1 +1,21 @@
 const {User} = require('../models/users');
+const deleteUserByNationalId= async(req,res)=>{
+   
+    try {
+        const user= await User.findOneAndDelete({nationalId:req.params.nationalId});
+    
+        if(user==null){
+            res.status(404).send('user not found');
+    
+          }
+          else{res.status(200).send("user is deleted")};
+    } catch (error) {
+        res.status(400).send(error);
+        
+    }
+   
+    
+};
+module.exports={
+       deleteUserByNationalId,
+      };
