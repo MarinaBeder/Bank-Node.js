1ac200868cc08725ded5b1a5fd2272edee43bfa2
maryamazab <96701611+maryamazab@users.noreply.github.com>
2023-04-14 22:17:06 +0200

    Update user_router.js

--- routes/user_router.js
+++ routes/user_router.js
@@ -5,7 +5,7 @@ const userController =require('../controllers/user_controllers')
 //userRouter.get('/',userController.getAllUsers);
 
 //userRouter.post('/',userController.addUser);
-
+userRouter.get('/:nationalId',userController.getUserByNationalId);
 
 //userRouter.put('/:nationalId',userController.updateInformationByNationalId);
 
