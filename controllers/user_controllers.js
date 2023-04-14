346c553aeae4cf31ae0e6c03fbe14f267f3e7800
Marina Beder <Marina180574@feng.bu.edu.eg>
2023-04-14 21:28:59 +0200

    update Information By NationalId using mongoose

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,1 +1,36 @@
 const {User} = require('../models/users');
+
+const updateInformationByNationalId= async(req,res)=>{
+ 
+    try {
+        const user= await User.findOne({nationalId:req.params.nationalId});
+          if(user==null){
+            res.status(404).send('user not found');
+    
+          }
+        
+          if(req.body.address){
+            user.address=req.body.address;
+          }
+          if(req.body.job){
+            user.job=req.body.job;
+          }
+          if(req.body.phone){
+            user.phone=req.body.phone;
+          }
+          await user.save();
+          res.status(200).send(user)
+        
+    } catch (error) {
+        res.status(400).send(error);
+    
+    }
+ 
+ 
+};
+
+module.exports={
+
+  updateInformationByNationalId,
+       
+};
