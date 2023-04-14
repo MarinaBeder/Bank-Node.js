27626964c8d934848baa4ae489405130773519b4
demiana55samir <65955978+demiana55samir@users.noreply.github.com>
2023-04-14 21:24:31 +0200

    Update user_controllers.js

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,1 +1,18 @@
 const {User} = require('../models/users');
+
+const addUser=async (req,res)=>{
+    try {      
+        const user =await User.create(req.body);
+
+  res.status(200).send(user);
+      
+  } catch (error) {
+      res.status(400).send(error);
+  }
+  
+
+ };
+
+module.exports={
+   addUser,
+      };
