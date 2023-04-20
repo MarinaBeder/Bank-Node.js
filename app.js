3f2aafd7040a97fd298c858a5fe0b7b09eb33595
Marina Beder <Marina180574@feng.bu.edu.eg>
2023-04-20 11:11:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ const express =require("express");
 const app =express();
 app.use(express.json());
 const {userRouter}=require('./routes/user_router');
+const {userRouter2}=require('./routes/transacation');
 app.use('/api/user/',userRouter); 
+app.use('/api/transaction/',userRouter2); 
 module.exports={app}
