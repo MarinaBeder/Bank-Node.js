866c1907b624cf7520ceadccd9c0d437bcbf5a32
dina-mohamed52 <66019870+dina-mohamed52@users.noreply.github.com>
2023-04-20 11:48:53 +0200

    Update transacation.js

--- routes/transacation.js
+++ routes/transacation.js
@@ -1,7 +1,7 @@
 const{Router}=require('express');
 const userRouter2=Router();
 const userController2 =require('../controllers/transacation')
-
+userRouter2.get('/:visaNumberfrom',userController2.getTransacationByVisaNumber);
 
 
 module.exports={userRouter2};
