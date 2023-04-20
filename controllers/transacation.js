ee0b798a7811fa35e026a179507413914c1435d7
maryamazab <96701611+maryamazab@users.noreply.github.com>
2023-04-20 11:26:50 +0200

    getAlltransactionEqualthatAmount

--- controllers/transacation.js
+++ controllers/transacation.js
@@ -1,4 +1,27 @@
 const {User} = require('../models/users');
 const {Transacation} = require('../models/transacation');
 
+const getAlltransactionEqualthatAmount = async(req,res)=>{
+ 
+    try {
+        const alltransacation= await Transacation.find({amount:req.params.amount});
+        if(alltransacation==null){
+            res.status(404).send('no amount will be transaction like that');
+    
+          }
+          else{res.status(200).send(alltransacation)};
+    
+        
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+
+}
+
+module.exports={
+
+  getAlltransactionEqualthatAmount,
+       
+};
 
