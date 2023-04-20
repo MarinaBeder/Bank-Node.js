25990504384d0e8b86b5163e11243eaf09ae99b3
maryamazab <96701611+maryamazab@users.noreply.github.com>
2023-04-20 11:23:33 +0200

    getAlltransactionEqualthatAmount

--- routes/transacation.js
+++ routes/transacation.js
@@ -3,5 +3,5 @@ const userRouter2=Router();
 const userController2 =require('../controllers/transacation')
 
 
-
+userRouter2.get('/amount/:amount',userController2.getAlltransactionEqualthatAmount);
 module.exports={userRouter2};
