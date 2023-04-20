65a5059063686603b8a90a6231296f9d6958a5ce
demiana55samir <65955978+demiana55samir@users.noreply.github.com>
2023-04-20 11:21:25 +0200

    Update transacation.js

--- routes/transacation.js
+++ routes/transacation.js
@@ -2,6 +2,6 @@ const{Router}=require('express');
 const userRouter2=Router();
 const userController2 =require('../controllers/transacation')
 
-
+userRouter2.get('/',userController2.getAlltranscation);
 
 module.exports={userRouter2};
