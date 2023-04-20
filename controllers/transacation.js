8d32d77817a054f1aba3c1793e1bb6a9a4b302d5
demiana55samir <65955978+demiana55samir@users.noreply.github.com>
2023-04-20 11:26:06 +0200

    Update transacation.js

--- controllers/transacation.js
+++ controllers/transacation.js
@@ -1,4 +1,18 @@
 const {User} = require('../models/users');
 const {Transacation} = require('../models/transacation');
 
+const getAlltranscation = async (req,res)=>{
+    try {
+      const alltransacation= await Transacation.find({});
+    res.status(200).send(alltransacation);
+        
+    } catch (error) {
+        res.status(400).send(error);
+    }
+    
+};
 
+module.exports={
+  getAlltranscation,
+       
+};
